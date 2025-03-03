b457ec674613e06c7b171fb13f0852332dc428e5
Debaditya <debadityamohankudo@gmail.com>
2025-03-03 08:24:00 +0530

    Final updates and fixes

--- myproject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import fetch_and_save_stock_data
-
-
-urlpatterns = [
-    path('fetch-stock-data/', fetch_and_save_stock_data, name='fetch_and_save_stock_data'),  # Correct URL pattern
-]
